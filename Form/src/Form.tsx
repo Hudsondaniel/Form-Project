fa8e99a472ade0b370c71778b65723c70c5bab4c
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-12-10 17:51:08 +0530

    first commit

---
+++ Form/src/Form.tsx
@@ -0,0 +1,15 @@
+
+function Form() {
+
+
+  return (
+      <div className="grid grid-cols-2 grid-rows-2 ml-28">
+        <div className="grid-1">Box 1</div>
+        <div className="grid-2">Box 2</div>
+        <div className="grid-3">Box 3</div>
+        <div className="grid-4">Box 4</div>
+      </div>
+  )
+}
+
+export default Form
