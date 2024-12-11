1fd998a22ed47531781833a8f2ab02f4ee131bc8
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-12-11 17:23:21 +0530

    Learning tailwind!

--- Form/src/Form.tsx
+++ Form/src/Form.tsx
@@ -6,10 +6,10 @@ function Form() {
     <div className="flex justify-center relative bg-cover bg-center" id='main-container'>
       <div className="flex items-center justify-center">
         <form noValidate className="bg-white p-8 shadow-md rounded-2xl">
-          <p>Sign Up Today!</p>
-          <p>Join Craft</p>
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
+          <p className="text-xl font-semibold pb-2">Sign Up</p>
+          <p className="text-gray-500 pb-2 text-sm font-semibold">Welcome To Craft!</p>
+          <div className="flex flex-col">
+            <label htmlFor="name">Full Name</label>
             <input type="text" id="name" name="name" required />
           </div>
 
