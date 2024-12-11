86f9cb561994f4a73c06417a3049f15499f39c0d
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-12-11 15:48:51 +0530

    Building the form component

--- Form/src/Form.tsx
+++ Form/src/Form.tsx
@@ -3,12 +3,47 @@ function Form() {
 
 
   return (
-      <div className="grid grid-cols-2 grid-rows-2 ml-28">
-        <div className="grid-1">Box 1</div>
-        <div className="grid-2">Box 2</div>
-        <div className="grid-3">Box 3</div>
-        <div className="grid-4">Box 4</div>
+    <div className="form">
+      <div className="inner-container">
+        <form noValidate>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="country">Country:</label>
+            <select id="country" name="country" required>
+              <option value="">Select your country</option>
+              <option value="us">United States</option>
+              <option value="in">India</option>
+              <option value="ca">Canada</option>
+              <option value="uk">United Kingdom</option>
+              <option value="au">Australia</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input type="password" id="confirmPassword" name="confirmPassword" required />
+          </div>
+
+          <div className="form-group">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
+    </div>
   )
 }
 
